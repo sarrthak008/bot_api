a9aaebd7065245f916d96406cc87df9089213360
sarrthak008 <sarthaknavale08@gmail.com>
2024-11-07 19:29:37 +0530

    use teligraf package

--- index.js
+++ index.js
@@ -8,28 +8,12 @@ import cors from 'cors'
 import { Telegraf } from 'telegraf';
 
 config()
-//connectDb()
+connectDb()
 
 app.use(cors())
 
 const bot = new Telegraf('7950790583:AAEagDNrVgGgjrWav8DR9TamnqFHsbinZJU')
 bot.start((ctx) => ctx.reply('Welcome'))
-
-app.get('/helth', (req,res)=>{
-    res.json({
-        data:'hey master server is runnig healthy...🏃‍♀️',
-        status:'200'
-    })
-})
-
-app.get('/messages',async (req,res)=>{
-    let allMessage = await message.find({})
-    if(allMessage){
-        bot.command('message', (ctx) => ctx.reply(allMessage))
-    }else{
-        res.send('not found yet or some thing went wrong')
-    }
-})
 
 
 
